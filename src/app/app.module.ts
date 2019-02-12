2dc87b066fe24b12dce5e6183e402b422f92b6e9
Leon Hunter <leonhunter@Leons-MacBook-Pro-2.local>
2019-02-12 15:52:50 -0500

    new changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TextBoxComponent } from './text-box/text-box.component';
+import { SearchComponent } from './search/search.component';
+import { CarImageComponent } from './car-image/car-image.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextBoxComponent,
+    SearchComponent,
+    CarImageComponent
   ],
   imports: [
     BrowserModule

134d45ac9582c4f5ecfcbb25f9f68e4008b2a8cd
Leon Hunter <leonhunter@Leons-MacBook-Pro-2.local>
2019-02-18 15:54:38 -0500

    new changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { TextBoxComponent } from './text-box/text-box.component';
 import { SearchComponent } from './search/search.component';
 import { CarImageComponent } from './car-image/car-image.component';
+import { RadioButtonComponent } from './radio-button/radio-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TextBoxComponent,
     SearchComponent,
-    CarImageComponent
+    CarImageComponent,
+    RadioButtonComponent,
+    NgbModule
   ],
   imports: [
-    BrowserModule
+    BrowserModule, NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
